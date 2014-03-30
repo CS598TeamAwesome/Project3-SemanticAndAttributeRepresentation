65ce35f482043a850b67ff6767fca938e002cf7f
Max Ehrlich <max.ehr@gmail.com>
2014-03-29 20:10:44 -0400

    Added Linux support to grid search, made search broader and added fine searching as a command line option

--- Source/Data/GenerateGridSearch.cpp
+++ Source/Data/GenerateGridSearch.cpp
@@ -8,36 +8,55 @@
 #include <time.h>
 #include <vector>
 
+#if !defined(WIN32) && !defined(WIN64)
+#   define LINUX
+#endif
+
 int main(int argc, char **argv){
-    std::string command1 = "svm-train -v 5 -w1 ";
+    std::string command1 = "svm-train -w1 ";
     std::string command2 = " -g ";
-    std::string command3 = " scaled_svm_train_Africans";
+    std::string command3 = " " + std::string(argv[1]);
 
-    //std::array<double, 9> gamma = {-13, -11, -9, -7, -5, -3, -1, 1, 3};
-    //std::array<double, 11> C = {-5, -3, -1, 1, 3, 5, 7, 9, 11, 13, 15};
-
-    double gamma_start = -13;
-    double gamma_end = 3;
+    double gamma_start = -100;
+    double gamma_end = 100;
     double gamma_increment = 2;
 
-    double C_start = -5;
-    double C_end = 15;
+    double C_start = -100;
+    double C_end = 100;
     double C_increment = 2;
-
-    std::ofstream outfile("coarse_grid_search.bat");
+    
     std::string results = " >> coarse_grid_search_results.out";
-/*
-    double gamma_start = -4;
-    double gamma_end = -3.5;
-    double gamma_increment = 0.05;
-
-    double C_start = .5;
-    double C_end = 1;
-    double C_increment = .05;
-
-    std::ofstream outfile("fine_grid_search.bat");
-    std::string results = " >> fine_grid_search_results.out";
-*/
+    std::ofstream outfile;
+    
+    if(std::string(argv[3]) == "fine")
+    {
+        gamma_start = atof(argv[4]);
+        gamma_end = atof(argv[5]);
+        gamma_increment = 0.05;
+    
+        C_start = atof(argv[6]);
+        C_end = atof(argv[7]);
+        C_increment = .05;
+    
+#   ifdef LINUX   
+        outfile.open("fine_grid_search");
+        outfile << "#!/bin/sh" << std::endl;
+#   else
+        std::ofstream outfile("fine_grid_search.bat");
+#   endif
+        
+        results = " >> fine_grid_search_results.out";
+    }
+    else
+    {
+#   ifdef LINUX   
+        outfile.open("coarse_grid_search");
+        outfile << "#!/bin/sh" << std::endl;
+#   else
+        std::ofstream outfile("coarse_grid_search.bat");
+#   endif
+    }
+    
     double gamma_current = gamma_start;
     while(gamma_current <= gamma_end){
         double C_current = C_start;
@@ -47,6 +66,7 @@ int main(int argc, char **argv){
             std::string s = convert.str();
             outfile << "echo " << s << results << std::endl;
             outfile << s << results << std::endl;
+            outfile << "svm-predict" << " " << argv[2] << command3 << ".model results.out" << results << std::endl;
             C_current += C_increment;
         }
         gamma_current += gamma_increment;
